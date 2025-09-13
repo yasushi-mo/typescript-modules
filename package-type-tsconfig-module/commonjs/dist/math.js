e14b9bfc1e95687fe6619c0a210d7911d634daad
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-09-13 12:06:09 +0900

    Add: compile commonjs commonjs

---
+++ package-type-tsconfig-module/commonjs/dist/math.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiply = exports.add = void 0;
+const add = (a, b) => {
+    return a + b;
+};
+exports.add = add;
+const multiply = (a, b) => {
+    return a * b;
+};
+exports.multiply = multiply;
